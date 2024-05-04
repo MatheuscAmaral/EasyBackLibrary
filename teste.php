a569045703ad068259186670be0b9756855413a0
Matheus Freitas <matheus.freitas@aglets.com.br>
2024-05-04 20:53:48 -0300

    feat: atualização dados teste

--- teste.php
+++ teste.php
@@ -3,10 +3,15 @@ require_once('database.php');
 require_once('index.php');
 
 $response = array(
-    'message' => 'Este é um exemplo de resposta do PHP.',
-    'data' => array(
-        'foo' => 'bar'
-    )
+    'Aluno' => [
+        'Derick Lucas Alves Rodrigues',
+        'Matheus Antônio Valentin Freitas',
+        'Matheus Campos do Amaral',
+        'Rodrigo Mota Proton Campos',
+        'Vinicius Celio Fontes Ribeiro'
+    ],
+    'Professor' => 'Luciana Mara Freitas Diniz',
+    'Materia' => 'Trabalho Interdiciplinar: Aplicações para Processos de Negócios'
 );
 
 echo json_encode($response);
