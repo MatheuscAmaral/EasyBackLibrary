01bfd3fcdfad075a91e5b16ef6a27bc133719fbb
Matheus Freitas <matheusvalentinfreitas@gmail.com>
2024-05-19 09:38:04 -0300

    fix: deleta usuario

--- usuario/deleteUsuario.php
+++ usuario/deleteUsuario.php
@@ -1,18 +1,48 @@
 <?php
 require_once('../database.php');
 require_once('../index.php');
+require_once('../classes/funcoesValidadoras.php');
+$response = new Response();
 
 $data = json_decode(file_get_contents("php://input"), true);
 
-$cpf = $data['cpf'];
+$cpf = $data['cpf'] ?? null;
 
-$sql = "DELETE FROM usuario
+try {
+    $cpf = Filter::retornaCampoTratado($cpf, null, 14, 'CPF', false);
+
+    if (!$cpf['result']) {
+        throw new Exception($cpf['message'], 1);
+    } else {
+        $cpf = $cpf['string'];
+
+        $selectValidaCPF = "SELECT * FROM USUARIO WHERE CPF = '$cpf'";
+        $queryValidaCPF = $con->query($selectValidaCPF);
+        $respostaValidaCPF = $queryValidaCPF->fetch(PDO::FETCH_ASSOC);
+
+        if (empty($respostaValidaCPF)) {
+            throw new Exception('O CPF informado não esta relacionado a nenhum usuario cadastrado no sistema.', 1);
+        }
+    }
+
+    $sql = "DELETE FROM usuario
         WHERE CPF = '$cpf'";
 
-if ($conn->query($sql) === TRUE) {
-    echo "Usuário deletado com sucesso!";
-} else {
-    echo "Erro ao deletar usuário: " . $conn->error;
+    if (!$con->query($sql)) {
+        throw new Exception("Ocorreu um erro ao tentar excluir o usuario", 1);
+    } else {
+        $response->setMessage("Usuario deletado com sucesso.");
+        echo $response->jsonResponse();
+    }
+} catch (Exception $e) {
+    if ($e->getCode() == 1) {
+        $response->setStatus(400);
+        $response->setMessage($e->getMessage());
+    } else {
+        $response->setStatus(500);
+        $response->setMessage('Ocorreu um erro no processamento.');
+        $response->setMessageErro($e->getMessage());
+        $response->setSql($sql);
+    }
+    echo $response->jsonResponse();
 }
-
-$conn->close();
